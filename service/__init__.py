cd962b16814894730117d6403db2895f351aa446
PaarkaviD <paarkavidhanasekar1997@gmail.com>
2024-07-05 08:26:18 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -6,18 +6,21 @@ and SQL database
 """
 import sys
 from flask import Flask
-from service import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
+from service.config import Config
 from service.common import log_handlers
 
-# Create Flask application
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(Config)
 
-# Import the routes After the Flask app is created
+# Initialize extensions
+db = SQLAlchemy(app)
+talisman = Talisman(app)  # Initialize Talisman after app creation
+
+# Import routes, models, and other modules after the app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
-
-# pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,12 +12,20 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from flask_talisman import Talisman
+from service import talisman  # Import talisman from service package
 
+# Set up the database URI
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
+# Define the base URL and HTTPS environment
 BASE_URL = "/accounts"
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+
+# Initialize Talisman
+talisman = Talisman(app)
 
 
 ######################################################################
@@ -34,16 +42,17 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False  # Disable forced HTTPS for testing
 
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
+        pass
 
     def setUp(self):
         """Runs before each test"""
         db.session.query(Account).delete()  # clean up the last tests
         db.session.commit()
-
         self.client = app.test_client()
 
     def tearDown(self):
@@ -53,7 +62,6 @@ class TestAccountService(TestCase):
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
-
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -73,7 +81,6 @@ class TestAccountService(TestCase):
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
-
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
@@ -133,23 +140,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-    ######################################################################
-    # READ AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
-        app.logger.info("Request to read an Account with id: %s", account_id)
-
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-        return account.serialize(), status.HTTP_200_OK
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
@@ -163,28 +153,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-    ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
-    @app.route("/accounts", methods=["GET"])
-    def list_accounts():
-        """
-        List all Accounts
-        This endpoint will list all Accounts
-        """
-        app.logger.info("Request to list Accounts")
-
-        accounts = Account.all()
-        account_list = [account.serialize() for account in accounts]
-
-        app.logger.info("Returning [%s] accounts", len(account_list))
-        return jsonify(account_list), status.HTTP_200_OK
-
-    def test_get_account_not_found(self):
-        """It should not List all accounts that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -199,61 +167,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
-        """
-        Update an Account
-        This endpoint will update an Account based on the posted data
-        """
-        app.logger.info("Request to update an Account with id: %s", account_id)
-
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-        account.deserialize(request.get_json())
-        account.update()
-
-        return account.serialize(), status.HTTP_200_OK
-
-    def test_update_account(self):
-        """It should Update an existing Account"""
-        # create an Account to update
-        test_account = AccountFactory()
-        resp = self.client.post(BASE_URL, json=test_account.serialize())
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-
-        # update the account
-        new_account = resp.get_json()
-        new_account["name"] = "Something Known"
-        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")
-
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
-        """
-        Update an Account
-        This endpoint will update an Account based on the posted data
-        """
-        app.logger.info("Request to update an Account with id: %s", account_id)
-
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-        account.deserialize(request.get_json())
-        account.update()
-
-        return account.serialize(), status.HTTP_200_OK
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
@@ -266,19 +179,19 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
-        account = Account.find(account_id)
-        if account:
-            account.delete()
 
-        return "", status.HTTP_204_NO_CONTENT
+if __name__ == '__main__':
+    unittest.main()
